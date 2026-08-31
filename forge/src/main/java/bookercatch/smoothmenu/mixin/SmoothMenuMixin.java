f0d1403c6cbd7938bf7a77f10ec534183758293c
Simon Lemay <simon.lemay@bell.net>
2026-08-31 10:17:52 -0400

    Add pause menu framerate support

--- forge/src/main/java/bookercatch/smoothmenu/mixin/SmoothMenuMixin.java
+++ forge/src/main/java/bookercatch/smoothmenu/mixin/SmoothMenuMixin.java
@@ -1,6 +1,8 @@
 package bookercatch.smoothmenu.mixin;
 
 import com.mojang.blaze3d.platform.FramerateLimitTracker;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,14 +12,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = FramerateLimitTracker.class, remap = false)
 public abstract class SmoothMenuMixin {
     @Shadow(remap = false)
+    @Final
+    private Minecraft minecraft;
+
+    @Shadow(remap = false)
     private int framerateLimit;
 
     @Shadow(remap = false)
     public abstract FramerateLimitTracker.FramerateThrottleReason getThrottleReason();
 
     @Inject(method = "getFramerateLimit", at = @At("HEAD"), cancellable = true, remap = false)
-    private void smoothmenu$uncapOutOfLevelMenu(CallbackInfoReturnable<Integer> cir) {
-        if (this.getThrottleReason() == FramerateLimitTracker.FramerateThrottleReason.OUT_OF_LEVEL_MENU) {
+    private void smoothmenu$uncapMenu(CallbackInfoReturnable<Integer> cir) {
+        if (this.getThrottleReason() == FramerateLimitTracker.FramerateThrottleReason.OUT_OF_LEVEL_MENU
+                || this.minecraft.screen != null && this.minecraft.screen.isPauseScreen()) {
             cir.setReturnValue(this.framerateLimit);
         }
     }
