785c201b982e5487ccbe67eea24f7bbaeba80e7d
Simon Lemay <simon.lemay@bell.net>
2026-08-31 10:50:43 -0400

    Fix pause detection for Minecraft 26.2

---
+++ quilt/src/main/java/bookercatch/smoothmenu/mixin/MinecraftAccessor.java
@@ -0,0 +1,12 @@
+package bookercatch.smoothmenu.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Minecraft.class)
+public interface MinecraftAccessor {
+    @Accessor("gui")
+    Gui smoothmenu$getGui();
+}
